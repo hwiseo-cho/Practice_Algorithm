5b13b2d6415b5004541f7fa0df67d7180bf01131
hwiseo-cho <gnltj37@naver.com>
2020-09-23 22:44:26 +0900

    2020-09-23 - 문제 1

--- src/com/my/practice/algo1/Prac1.java
+++ src/com/my/practice/algo1/Prac1.java
@@ -1,5 +1,18 @@
 package com.my.practice.algo1;
 
+
+/*
+	쉬운거부터 시작
+*/
 public class Prac1 {
 
+	public int p1(int a, int b) {
+		int result = 0;
+		result = a+b;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Prac1().p1(1, 3));
+	}
 }
