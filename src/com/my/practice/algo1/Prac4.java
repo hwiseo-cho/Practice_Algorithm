142fff168a5e0e149ec3b68d7a1570c2fd0dc921
hwiseo-cho <gnltj37@naver.com>
2020-10-06 23:40:32 +0900

    2020-10-06 - 추가

---
+++ src/com/my/practice/algo1/Prac4.java
@@ -0,0 +1,14 @@
+package com.my.practice.algo1;
+
+import java.util.Scanner;
+
+public class Prac4 {
+
+	public static void main(String[] args) {
+		new Prac4().menu();
+	}
+	
+	public void menu() {
+		Scanner sc = new Scanner(System.in);
+	}
+}
