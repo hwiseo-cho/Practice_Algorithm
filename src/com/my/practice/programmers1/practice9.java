7a31012ccd2b40c2c608771eb63700697bd00c46
hwiseo-cho <gnltj37@naver.com>
2021-11-10 19:03:45 +0900

    마스킹 처리 코딩테스트

--- src/com/my/practice/programmers1/practice9.java
+++ src/com/my/practice/programmers1/practice9.java
@@ -5,9 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 /*
- *2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
-
-입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요. */
+ 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.*/
 public class practice9 {
 	
 	public static void main(String[] args) {
@@ -28,7 +27,12 @@ public class practice9 {
 	
 	// 내꺼
 	public static String mySolution(String phone_number) {
-		return "";
+		String result = "";
+		for(int i=0; i<phone_number.length()-4; i++) {
+			result += "*";
+		}
+		result += phone_number.substring(phone_number.length()-4, phone_number.length());
+		return result;
 	}
 		
 }
