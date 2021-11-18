e51eecdf15a9f8249a964cba1917820e0825deae
hwiseo-cho <gnltj37@naver.com>
2021-11-18 19:01:46 +0900

    짝지어 제거 코딩테스트

--- src/com/my/practice/programmers1/practice10.java
+++ src/com/my/practice/programmers1/practice10.java
@@ -11,14 +11,14 @@ import java.util.*;
 public class practice10 {
 	
 	public static void main(String[] args) {
-		String s = "cdcd";
+		String s = "cdcdd";
 		System.out.println(solution(s));
 	}
 	
 	
 	 public static int solution(String s) {
 		 Stack<Character> stack = new Stack<>();
-         
+          
          for(char c : s.toCharArray()) 
            if(!stack.isEmpty() && stack.peek() == c) stack.pop();
            else stack.push(c);
