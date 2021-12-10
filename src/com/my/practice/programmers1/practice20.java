5abc2d889c678c1750dfac463959914ac494d755
hwiseo-cho <57250330+hwiseo-cho@users.noreply.github.com>
2021-12-10 19:07:34 +0900

    다리를 지나가는 트럭 코딩테스트

--- src/com/my/practice/programmers1/practice20.java
+++ src/com/my/practice/programmers1/practice20.java
@@ -15,7 +15,7 @@ public class practice20 {
 		int[] truck_weights = {7,4,5,6};
 		System.out.println(solution(bridge_length,weight,truck_weights));
 	}
-	  
+	     
 	
 	public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0; 
