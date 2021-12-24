ee91db661c46cf94cb0568ca849a4934136b6225
hwiseo-cho <57250330+hwiseo-cho@users.noreply.github.com>
2021-12-24 18:48:04 +0900

    책 페이지 코딩테스트

--- src/com/my/practice/beakjoon/practice1.java
+++ src/com/my/practice/beakjoon/practice1.java
@@ -16,7 +16,7 @@ public class practice1 {
 		int n = sc.nextInt();
 		
 		Map<Integer,Integer> map = new HashMap<>();
-		map.put(0,0);
+		map.put(0,0); 
 		map.put(1,0);
 		map.put(2,0);
 		map.put(3,0);
