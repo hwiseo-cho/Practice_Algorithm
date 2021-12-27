9d0c0ed23a51533f4d5a0e79ecf1fd393704ea60
hwiseo-cho <57250330+hwiseo-cho@users.noreply.github.com>
2021-12-27 18:48:28 +0900

    책 페이지 코딩테스트

--- src/com/my/practice/beakjoon/practice1.java
+++ src/com/my/practice/beakjoon/practice1.java
@@ -7,7 +7,7 @@ import java.util.stream.LongStream;
  *	카카오톡 오픈채팅방에서는 친구가 아닌 사람들과 대화를 할 수 있는데, 본래 닉네임이 아닌 가상의 닉네임을 사용하여 채팅방에 들어갈 수 있다.
 
  *
- **/
+ **/ 
 public class practice1 {
 	
 	public static void main(String[] args) {
